e2dd40af42edb8241ed58a729c53e34b6ebfa771
AditiRaySingh <aditiraysingh2005@gmail.com>
2026-01-23 02:40:45 +0530

    updated logic

--- assignment-2/question2/stringUtils.js
+++ assignment-2/question2/stringUtils.js
@@ -1,32 +1,25 @@
-// Capitalize first letter of each word
 function capitalize(str) {
-  return str
-    .split(" ")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+  return str.toUpperCase();
 }
 
-// Reverse a string
-function reverseString(str) {
+function reverse(str) {
   return str.split("").reverse().join("");
 }
 
-// Count vowels in a string
 function countVowels(str) {
-  const vowels = "aeiouAEIOU";
   let count = 0;
+  let vowels = "aeiouAEIOU";
 
-  for (let char of str) {
-    if (vowels.includes(char)) {
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
       count++;
     }
   }
   return count;
 }
 
-// Export all functions
 module.exports = {
   capitalize,
-  reverseString,
+  reverse,
   countVowels
 };
