9532dffdcbdbfa15e1535861653448e1ae3206d4
AditiRaySingh <aditiraysingh2005@gmail.com>
2026-03-26 23:06:27 +0530

    Added ASSIGNMENT-6

--- assignment-2/question2/app.js
+++
@@ -1,5 +0,0 @@
-const stringUtils = require("./stringUtils");
-
-console.log(stringUtils.capitalize("hello"));   // HELLO
-console.log(stringUtils.reverse("hello"));      // olleh
-console.log(stringUtils.countVowels("hello"));  // 2
